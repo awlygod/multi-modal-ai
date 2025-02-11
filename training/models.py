c0846d9d0c60450875df0043a958d8918bbb8571
awlygod <surajpatitripathi@gmail.com>
2025-02-11 11:00:38 +0530

    11/02 def evaluate

--- training/models.py
+++ training/models.py
@@ -3,6 +3,10 @@ import torch.nn as nn
 from transformers import BertModel
 import torchvision.models as vision_models
 from meld_dataset import MELDDataset
+from sklearn.metrics import precision_score, accuracy_score
+from torch.utils.tensorboard import SummaryWriter
+import os
+from datetime import datetime
 
 # the code is defining a text encoder that uses BERT (a specific type of transformer model )
 class TextEncoder(nn.Module):
@@ -144,7 +148,203 @@ class MultimodalSentimentModel(nn.Module):
             'emotions' : emotion_output,
             'sentiments': sentiment_output
         }
-if __name__ == "__main__":
+
+
+
+class MultimodalTrainer:
+            def __init__(self,model,train_loader, val_loader):
+                self.model = model
+                self.train_loader = train_loader
+                self.val_loader = val_loader
+
+                # Log datatset sized 
+                train_size = len(train_loader.dataset)
+                val_size = len(val_loader.dataset)
+                print("\nDataset sizes:")
+                print(f"Training samples: {train_size:,}")
+                print(f"Validation samples: {val_size:,}")
+                print(f"Batches per epoch: {len(train_loader):,}")
+
+                
+                timestamp = datetime.now().strftime('%b%d_%H-%M-%S')  # Dec17_14-22-35
+                base_dir = '/opt/ml/output/tensorboard' if 'SM_MODEL_DIR' in os.environ else 'runs'
+                log_dir = f"{base_dir}/run_{timestamp}"
+                self.writer = SummaryWriter(log_dir=log_dir)
+                self.global_step = 0
+
+                # Very high :1 , high: 0.1-0.01, medium: 1e-1, low: 1e-4, very low: 1e-5
+                self.optimizer = torch.optim.Adam([
+                    {'params': model.text_encoder.paramters(), 'lr': 8e-6},
+                    {'params': model.video_encoder.paramters(), 'lr': 8e-5},
+                    {'params': model.audio_encoder.paramters(), 'lr': 8e-5},
+                    {'params': model.fusion_layer.paramters(), 'lr': 5e-4},
+                    {'params': model.emotion_classifier.paramters(), 'lr': 5e-6},
+                    {'params': model.sentiment_classifier.paramters(), 'lr': 5e-4}
+                
+                ], weight_decay = 1e-5)
+
+                self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+                    self.optimizer,
+                    mode = "min",
+                    factor = 0.1,
+                    patience= 2
+                )
+                self.emotion_criterion = nn.CrossEntropyLoss(
+                    label_smoothing=0.05
+                )
+                self.sentiment_criterion = nn.CrossEntropyLoss(
+                    label_smoothing=0.05
+                )
+            def log_metrics(self, losses, metrics=None, phase="train"):
+                if phase == "train":
+                    self.current_train_losses = losses
+                else:  # Validation phase
+                    self.writer.add_scalar(
+                        'loss/total/train', self.current_train_losses['total'], self.global_step)
+                    self.writer.add_scalar(
+                        'loss/total/val', losses['total'], self.global_step)
+
+                    self.writer.add_scalar(
+                        'loss/emotion/train', self.current_train_losses['emotion'], self.global_step)
+                    self.writer.add_scalar(
+                        'loss/emotion/val', losses['emotion'], self.global_step)
+
+                    self.writer.add_scalar(
+                        'loss/sentiment/train', self.current_train_losses['sentiment'], self.global_step)
+                    self.writer.add_scalar(
+                        'loss/sentiment/val', losses['sentiment'], self.global_step)
+
+                if metrics:
+                    self.writer.add_scalar(
+                        f'{phase}/emotion_precision', metrics['emotion_precision'], self.global_step)
+                    self.writer.add_scalar(
+                        f'{phase}/emotion_accuracy', metrics['emotion_accuracy'], self.global_step)
+                    self.writer.add_scalar(
+                        f'{phase}/sentiment_precision', metrics['sentiment_precision'], self.global_step)
+                    self.writer.add_scalar(
+                        f'{phase}/sentiment_accuracy', metrics['sentiment_accuracy'], self.global_step)  
+
+            def train_epoch(self):
+                self.model.train()
+                running_loss = {'total':0, 'emotion':0, 'sentiment': 0}
+                for batch in self.train_loader:
+                    device = next(self.model.parameters()).device
+                    text_inputs = {
+                        'input_ids': batch['text_input']['input_ids'].to(device),
+                        'attention_mask': batch['text_inpt']['attention_mask'].to(device)
+                    }
+                    video_frames = batch['video_frames'].to(device)
+                    audio_features = batch['audio_frames'].to(device)
+                    emotion_labels = batch['emotion_label'].to(device)
+                    sentiment_labels = batch['sentiment_label'].to(device)
+
+                    # Zero gradient
+                    self.optimizer.zero_grad()
+
+                    # Forward pass
+                    outputs = self.odel(text_inputs, video_frames, audio_features)
+
+                    # Calculate losses using raw logics
+                    emotion_loss = self.emotion_criterion(
+                        outputs["emotion"], emotion_labels)
+                    sentiment_loss = self.sentiment_criterion(
+                        outputs["emotion"], sentiment_labels)
+                    total_loss = emotion_loss + sentiment_loss
+
+                    # Backward pass. Calculate gradients
+                    total_loss.backward()
+
+                    torch.nn.utils.clip_grad_norm_(
+                        self.model.parameters(), max_norm =1.0)
+
+                    self.optimizer.step()
+
+                    # Track losses 
+                    running_loss['total'] += total_loss.item()
+                    running_loss['emotion'] += emotion_loss.item()
+                    running_loss['sentiment'] += sentiment_loss.item()
+
+                    self.log_metrics({
+                        'total': total_loss.item(),
+                        'emotion': emotion_loss.item(),
+                        'sentiment': sentiment_loss.item()
+                    })
+                    self.global_step += 1
+
+                return {k: v/len(self.train_loader) for k,v in running_loss.items()}
+
+
+            def evaluate(self, data_loader, phase= "val"):
+                self.model.eval()
+                losses = { 'total': 0, 'emotion': 0, 'sentiment': 0}
+                all_emotion_preds = []
+                all_emotion_labels = []
+                all_sentiment_preds = []
+                all_sentiment_labels = []
+
+                with torch.inference_model():
+                    for batch in data_loader:
+                        device = next(self.model.oarameters()).device
+                        text_inputs = {
+                            'input_ids': batch['text_inputs']['input_ids'].to(device),
+                            'attention_mask': batch['text_inputs']['attention_mask'].to(device)
+                        }
+                        video_frames = batch['video_frames'].to(device)
+                        audio_features = batch['audio_frames'].to(device)
+                        emotion_labels = batch['emotion_label'].to(device)
+                        sentiment_labels = batch['sentiment_label'].to(device)
+
+                        outputs = self.model(text_inputs, video_frames, audio_features)
+
+                        emotion_loss = self.emotion_criterion(
+                            outputs["emotions"], emotion_labels)
+                        sentiment_loss = self.sentiment_criterion(
+                            outputs["sentiments"], sentiment_labels)
+                        total_loss = emotion_loss + sentiment_loss
+                        
+                        all_emotion_preds.extend(
+                            outputs["emotions"].argmax(dim=1).cpu().numpy())
+                        all_emotion_labels.extend(emotion_labels.cpu().numpy())
+                        all_sentiment_preds.extend(
+                            outputs["sentiments"].argmax(dim=1).cpu().numpy())
+                        all_sentiment_labels.extend(sentiment_labels.cpu().numpy())
+
+                        losses['total'] += total_loss.item()
+                        losses['emotion'] += emotion_loss.item()
+                        losses['sentiment'] += sentiment_loss.item()
+                
+
+                avg_loss = {k: v/len(data_loader) for k, v in losses.items()} 
+
+                 # Compute the precision and accuracy
+                emotion_precision = precision_score(
+                    all_emotion_labels, all_emotion_preds, average='weighted')
+                emotion_accuracy = accuracy_score(
+                    all_emotion_labels, all_emotion_preds)
+                sentiment_precision = precision_score(
+                    all_sentiment_labels, all_sentiment_preds, average='weighted')
+                sentiment_accuracy = accuracy_score(
+                    all_sentiment_labels, all_sentiment_preds)
+
+                self.log_metrics(avg_loss, {
+                    'emotion_precision': emotion_precision,
+                    'emotion_accuracy': emotion_accuracy,
+                    'sentiment_precision': sentiment_precision,
+                    'sentiment_accuracy': sentiment_accuracy
+                }, phase=phase)
+
+                if phase == "val":
+                    self.scheduler.step(avg_loss['total'])
+
+                return avg_loss, {
+                    'emotion_precision': emotion_precision,
+                    'emotion_accuracy': emotion_accuracy,
+                    'sentiment_precision': sentiment_precision,
+                    'sentiment_accuracy': sentiment_accuracy
+                    }
+
+
+if __name__ == "__main__": 
     dataset = MELDDataset(
         '../dataset/train/train_sent_emo.csv', '../dataset/train/train_splits'
     )
